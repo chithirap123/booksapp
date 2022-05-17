de03ed85681760aa52388565bf21d163ee76fc37
chithirap123 <chithirakaringattil1@gmail.com>
2022-05-17 12:53:10 +0530

    books

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addbook from './Component/Addbook';
+import Searchstudent from './Component/Searchbook';
 
 function App() {
   return (
     <div>
 <Addbook/>
+<Searchstudent/>
     </div>
  
   );
---
+++ src/Component/Searchbook.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Searchbook = () => {
+  return (
+    <div>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label for="" className="label-formlabel">Bookstitle</label>
+            <input type="text" className="form-control"/>
+        </div>
+        <div>
+        <button className="btn btn-sucess">SUBMIT</button>
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Searchbook
